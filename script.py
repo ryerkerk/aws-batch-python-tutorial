0048c5c15e2d42a3ab03a61caafe7954bf376592
Matt Ryerkerk <ryerkerk@gmail.com>
2020-02-16 22:56:15 -0500

    Initial sample python script

---
+++ script.py
@@ -0,0 +1,27 @@
+import argparse
+import pickle
+
+def parse_arg():
+    """
+    This function parses command line arguments to this script
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--var_a", type=int, default=1)
+    parser.add_argument("--var_b", type=int, default=2)
+
+    params = vars(parser.parse_args())
+
+    return params
+
+if __name__ == "__main__":
+    params = parse_arg()  # Parse command line arguments
+
+    # Use input parameters to calculate a new result
+    a = params['var_a']
+    b = params['var_b']
+    c = a*b
+    print(c)
+    
+    # Write results to file
+    pickle.dump(c, open('results.p', 'wb'))
